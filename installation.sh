68ff5fef77b7b0ec1f94ad71927f294ab65259b4
Zhiyan Foo <zhiyanfoo@gmail.com>
2017-07-20 00:20:40 +0800

    fix copy bug

--- installation.sh
+++ installation.sh
@@ -16,7 +16,7 @@ fi
 if [ -e "/usr/local/bin/zen" ]; then
     echo "zen already exists in /usr/local/bin"
 else
-    cp zen.sh /usr/local/bin/zen
+    cp $HOME/tools/zen/zen.sh /usr/local/bin/zen
 fi
 
 echo "You have to manually create alias"
