b9f791701ed3625a67ad60e72b3cb0d06e08eeec
Zhiyan Foo <zhiyanfoo@gmail.com>
2018-09-04 22:33:30 +0900

    Modify dir based on executable name

--- zen.sh
+++ zen.sh
@@ -104,30 +104,45 @@ zen_use_else_create() {
     unset zen_new_env
 }
 
-if [ ! -d $zen_pyenv_dir ]; then
-    echo "Creating '$zen_pyenv_dir'"
-    mkdir $zen_pyenv_dir
+main() {
+    if [ ! -d $zen_pyenv_dir ]; then
+        echo "Creating '$zen_pyenv_dir'"
+        mkdir $zen_pyenv_dir
+    fi
+
+    if [ $# -eq 0 ]; then
+        zen_list
+    elif [ "$1" = "list" ]; then
+        zen_list
+    elif [ "$1" = "dir" ]; then
+        echo $zen_pyenv_dir
+    elif [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$1" = "help" ]; then
+        zen_help
+    elif [ "$1" = "create" ]; then
+        zen_create $2
+    elif [ "$1" = "new" ]; then
+        zen_new $2
+    elif [ "$1" = "remove" ]; then
+        zen_remove $2
+    elif [ "$1" = "use" ]; then
+        zen_use $2
+    else
+        zen_use_else_create $1
+    fi
+}
+
+
+if [ "$0" = "zen" ]; then
+    zen_pyenv_dir="$HOME/.zen"
+elif [ "$0" = "zen2" ]; then
+    zen_pyenv_dir="$HOME/.zen2"
+else
+    echo "Name of executable must be 'zen' or 'zen2'"
+    echo "Currently it is '$0'"
+    return 1
 fi
 
-if [ $# -eq 0 ]; then
-    zen_list
-elif [ "$1" = "list" ]; then
-    zen_list
-elif [ "$1" = "dir" ]; then
-    echo $zen_pyenv_dir
-elif [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$1" = "help" ]; then
-    zen_help
-elif [ "$1" = "create" ]; then
-    zen_create $2
-elif [ "$1" = "new" ]; then
-    zen_new $2
-elif [ "$1" = "remove" ]; then
-    zen_remove $2
-elif [ "$1" = "use" ]; then
-    zen_use $2
-else
-    zen_use_else_create $1
-fi
+main $@
 
 unset zen_pyenv_dir
 unset zen_pyenv_dir2
